6ec433c1f33d52bc769b6eada9ee3e9a94eac501
Benjamin Yang <benjamin.yang12@outlook.com>
2025-08-12 19:08:49 +1000

    fix user-agent

--- lib/downloader.ts
+++ lib/downloader.ts
@@ -63,7 +63,7 @@ export async function downloadGithub(name: string, version: string, repo: string
   try {
     const res = await fetch('https://api.github.com/repos/' + repo + '/releases/latest', {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (compatible; mc_server_plus_plus/1.0)'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
       }
     });
     if (!res.ok) {
