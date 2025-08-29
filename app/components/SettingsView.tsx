7814b434f6ae2fccea0216fa4de462d8b05aa56f
Benjamin Yang <benjamin.yang12@outlook.com>
2025-08-29 14:41:55 +1000

    add update function directly to settings view

--- app/components/SettingsView.tsx
+++ app/components/SettingsView.tsx
@@ -7,9 +7,38 @@ interface SettingsCardProps {
   onServerUpdate: (updatedServer: Partial<any>) => void;
 }
 
-export default function SettingsCard({ serverData, onServerUpdate }: SettingsCardProps) {
+export default function SettingsView({ serverData, onServerUpdate }: SettingsCardProps) {
   const [server, setServer] = useState(JSON.parse(serverData));
   const [versions, setVersions] = useState<any[]>([]);
+  const [serverStatus, setServerStatus] = useState<{updating?: boolean; msg?: string, err?: string}>({});
+
+  const handleServerUpdate = async () => {
+    try {
+      setServerStatus({updating: true, err: undefined, msg: undefined});
+
+      const res = await fetch (`/api/servers/${server.name}/server`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ server, build: server.build.toString() }),
+      });
+
+      if (!res.ok) {
+        const data = await res.json().catch(() => ({}));
+        throw new Error(data.error || 'unknown error');
+      }
+
+      const { newVersion } = await res.json();
+
+      setServerStatus({ updating: false, msg: (newVersion !== server.build)? 'successful update' :  'no new version' })
+      
+      const updatedServer = { ...server, build: newVersion };
+      setServer(updatedServer);
+      
+      onServerUpdate({ build: newVersion });
+    } catch (err: any) {
+      setServerStatus({ updating: false, err: err?.message || 'unknown error'})
+    }
+  }
 
   useEffect(() => {
     const getVersions = async () => {
@@ -43,7 +72,7 @@ export default function SettingsCard({ serverData, onServerUpdate }: SettingsCar
     newServer.version = version;
     setServer(newServer);
     onServerUpdate(newServer);
-    alert('To complete version change, go to software tab and update the server software.');
+    handleServerUpdate();
   }
 
   return (
@@ -68,6 +97,17 @@ export default function SettingsCard({ serverData, onServerUpdate }: SettingsCar
           >
             Save
           </button>
+          <div className="flex flex-col">
+            {serverStatus.updating && (
+              <label>Updating...</label>
+            )}
+            {serverStatus.msg && (
+              <label className="text-green-400">{serverStatus.msg}</label>
+            )}
+            {serverStatus.err && (
+              <label className="text-red-400">{serverStatus.err}</label>
+            )}
+          </div>
         </section>
         <label>
           {server.software}: {server.build}
